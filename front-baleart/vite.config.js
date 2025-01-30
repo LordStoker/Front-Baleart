3d71a6e9bf4da4e4d25a1717ec59ac6346f39240
LordStoker <tpc00@iesemilidarder.com>
2025-01-30 15:15:55 +0100

    .

--- front-baleart/vite.config.js
+++ front-baleart/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://baleart1.test',
+        target: 'http://baleart.test',
         changeOrigin: true,
         secure: false,
       },
